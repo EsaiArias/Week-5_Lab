7ebf5c4f49616053ff21fc30f8cd717ecb757ba3
EsaiArias <100735475+EsaiArias@users.noreply.github.com>
2023-03-07 05:18:44 +0000

    completed assignment

--- .ccls-cache/@home@runner@Week-5-EsaiArias/main.cpp
+++ .ccls-cache/@home@runner@Week-5-EsaiArias/main.cpp
@@ -6,55 +6,126 @@ Student last name: Arias
 #include <iostream>
 using namespace std;
 
-class Vehicle
-{
+class Vehicle {
 public:
+  void print() const;
+  Vehicle(int = 0, int = 0);
+
 private:
-};// Vehicle
+  int wheels;
+  int doors;
+}; // Vehicle
 
-
-class Car: public Vehicle
-{
+class Car : public Vehicle {
 public:
+  void print() const;
+  Car(int = 0, int = 0, int = 0, string = "");
+
 private:
-};// Car
+  int MPG;
+  string model;
+};
 
-
-class Truck: public Vehicle
-{
+class Truck : public Vehicle {
 public:
+  void print() const;
+  Truck(int = 0, int = 0, int = 0, int = 0, string = "");
+
 private:
-};// Truck
-
-
+  int MPG;
+  int tons;
+  string model;
+}; // Truck
 
 int main() {
-  // declare local variables
+  int MPG, tons, wheels, doors;
+  string model;
 
   cout << "Enter car details\n";
-
-
-  // declare Car object
-
+  cout << "Number of wheels: ";
+  cin >> wheels;
+  cout << "Number of doors: ";
+  cin >> doors;
+  cout << "MPG: ";
+  cin >> MPG;
+  cout << "Model name: ";
+  cin >> model; 
+    
+  Car Tesla (wheels, doors, MPG, model);// declare Car object
 
   cout << endl;
   cout << "Enter truck details\n";
-
-  // declare Truck object
-
+  cout << "Number of wheels: ";
+  cin >> wheels;
+  cout << "Number of doors: ";
+  cin >> doors;
+  cout << "MPG: ";
+  cin >> MPG;
+  cout << "How many tons can the truck carry: ";
+  cin >> tons;
+  cout << "Model name: ";
+  cin >> model; 
+  
+  Truck Tacoma (wheels, doors, MPG, tons, model);// declare Truck object
 
   cout << endl;
 
-  // call print function for your car and for your truck
-
+  Tesla.print();
+  Tacoma.print();// call print function for your car and for your truck
 
   cout << "\nGoodbye\n";
   return 0;
 }
+void Truck::print() const {
+  cout << model << " gets " << MPG << " miles per gallon, can carry " << tons << " tons, and has "; 
+  Vehicle::print();
+}
 
+Truck::Truck(int W, int D, int milesPerGallon, int T, string name)
+    : Vehicle(W, D) {
+  MPG = milesPerGallon;
+  tons = T;
+  model = name;
+}
+
+void Car::print() const {
+  cout << model << " gets " << MPG << " miles per gallon and has ";
+  Vehicle::print();
+}
+
+Car::Car(int W, int D, int milesPerGallon, string name) : Vehicle(W, D) {
+  MPG = milesPerGallon;
+  model = name;
+}
+
+void Vehicle::print() const {
+  cout << wheels << " wheels and " << doors << " doors.\n";
+}
+
+Vehicle::Vehicle(int W, int D) {
+  wheels = W;
+  doors = D;
+}
 /***************************************
 YOUR OUTPUT:
 
+Enter car details
+Number of wheels: 4
+Number of doors: 4
+MPG: 50
+Model name: tesla3
+
+Enter truck details
+Number of wheels: 8
+Number of doors: 4
+MPG: 20
+How many tons can the truck carry: 5
+Model name: tacoma
+
+tesla3 gets 50 miles per gallon and has 4 wheels and 4 doors.
+tacoma gets 20 miles per gallon, can carry 5 tons, and has 8 wheels and 4 doors.
+
+Goodbye
 
 SAMPLE OUTPUT:
 Enter car details
@@ -71,7 +142,8 @@ How many tons can the truck carry: 5
 Model name: Peterbilt
 
 Toyota gets 36 miles per gallon and has 4 wheels and 4 doors.
-Peterbilt gets 22 miles per gallon, can cary 5 tons, and has 18 wheels and 2 doors.
+Peterbilt gets 22 miles per gallon, can carry 5 tons, and has 18 wheels and 2
+doors.
 
 Goodbye
 ****************************************/

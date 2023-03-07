0484a854e5d8f01d4cdd84fa674736716439c57f
EsaiArias <100735475+EsaiArias@users.noreply.github.com>
2023-03-07 04:16:21 +0000

    Initial commit

---
+++ .ccls-cache/@home@runner@Week-5-EsaiArias/main.cpp
@@ -0,0 +1,77 @@
+/**********************
+Lab: Week 5
+Student first name: Esai
+Student last name: Arias
+***********************/
+#include <iostream>
+using namespace std;
+
+class Vehicle
+{
+public:
+private:
+};// Vehicle
+
+
+class Car: public Vehicle
+{
+public:
+private:
+};// Car
+
+
+class Truck: public Vehicle
+{
+public:
+private:
+};// Truck
+
+
+
+int main() {
+  // declare local variables
+
+  cout << "Enter car details\n";
+
+
+  // declare Car object
+
+
+  cout << endl;
+  cout << "Enter truck details\n";
+
+  // declare Truck object
+
+
+  cout << endl;
+
+  // call print function for your car and for your truck
+
+
+  cout << "\nGoodbye\n";
+  return 0;
+}
+
+/***************************************
+YOUR OUTPUT:
+
+
+SAMPLE OUTPUT:
+Enter car details
+Number of wheels: 4
+Number of doors: 4
+MPG: 36
+Model name: Toyota
+
+Enter truck details
+Number of wheels: 18
+Number of doors: 2
+MPG: 22
+How many tons can the truck carry: 5
+Model name: Peterbilt
+
+Toyota gets 36 miles per gallon and has 4 wheels and 4 doors.
+Peterbilt gets 22 miles per gallon, can cary 5 tons, and has 18 wheels and 2 doors.
+
+Goodbye
+****************************************/
---
+++ .ccls-cache/@home@runner@Week-5/main.cpp
@@ -0,0 +1,65 @@
+/**********************
+Lab: Week 5
+Student first name:
+Student last name:
+***********************/
+#include <iostream>
+using namespace std;
+
+// Vehicle
+
+
+// Car
+
+
+// Truck
+
+
+
+int main() {
+  // declare local variables
+
+  cout << "Enter car details\n";
+
+
+  // declare Car object
+
+
+  cout << endl;
+  cout << "Enter truck details\n";
+
+  // declare Truck object
+
+
+  cout << endl;
+
+  // call print function for your car and for your truck
+
+
+  cout << "\nGoodbye\n";
+  return 0;
+}
+
+/***************************************
+YOUR OUTPUT:
+
+
+SAMPLE OUTPUT:
+Enter car details
+Number of wheels: 4
+Number of doors: 4
+MPG: 36
+Model name: Toyota
+
+Enter truck details
+Number of wheels: 18
+Number of doors: 2
+MPG: 22
+How many tons can the truck carry: 5
+Model name: Peterbilt
+
+Toyota gets 36 miles per gallon and has 4 wheels and 4 doors.
+Peterbilt gets 22 miles per gallon, can cary 5 tons, and has 18 wheels and 2 doors.
+
+Goodbye
+****************************************/
